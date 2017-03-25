8af42d5736bae2c4417f4a310d382e9835cda3df
David <prprcupofcoffee@gmail.com>
2017-03-24 18:55:03 -0600

    663wk2 assignment updates

--- MSSE663/ThompsonD-LT2-AboutBio/createaccount.php
+++ MSSE663/ThompsonD-LT2-AboutBio/createaccount.php
@@ -1,38 +1,40 @@
 <?php
 include 'validate.php';
 
+$success = false;
+
 // make sure we were POSTed to
 //
-if (filter_input(INPUT_SERVER, "REQUEST_METHOD") != "POST") {
-    http_response_code(500);
-    return;
-}
-if (filter_input(INPUT_POST, "create-account") != "t") {
-    http_response_code(500);
-    return;
-}
+if (filter_input(INPUT_SERVER, "REQUEST_METHOD") != "POST" ||
+    filter_input(INPUT_POST, "create-account") != "t") {
+    $result = "There was an error creating the account.";
+} else {
+    $firstName = filter_input(INPUT_POST, "first-name");
+    $lastName = filter_input(INPUT_POST, "last-name");
+    $emailAddress = filter_input(INPUT_POST, "email-address");
+    $userName = filter_input(INPUT_POST, "user-name");
+    $password = filter_input(INPUT_POST, "password");
+    $confirmPassword = filter_input(INPUT_POST, "password-confirm");
 
-$firstName = filter_input(INPUT_POST, "first-name");
-$lastName = filter_input(INPUT_POST, "last-name");
-$emailAddress = filter_input(INPUT_POST, "email-address");
-$userName = filter_input(INPUT_POST, "user-name");
-$password = filter_input(INPUT_POST, "password");
-$confirmPassword = filter_input(INPUT_POST, "password-confirm");
+    $validator = new Validate;
 
-$validator = new Validate;
-
-if (!$validator->validateName($firstName) ||
-    !$validator->validateName($lastName) ||
-    !$validator->validateEmailAddress($emailAddress) ||
-    !$validator->validateUserName($userName) ||
-    !$validator->validatePassword($password) ||
-    !$validator->validateConfirmPassword($password, $confirmPassword)) {
-    $result = "invalidData";
-} else if (add_user($firstName, $lastName, $emailAddress, $userName, $password, $result)) {
-    $result = "success";
+    if (!$validator->validateName($firstName) ||
+            !$validator->validateName($lastName) ||
+            !$validator->validateEmailAddress($emailAddress) ||
+            !$validator->validateUserName($userName) ||
+            !$validator->validatePassword($password) ||
+            !$validator->validateConfirmPassword($password, $confirmPassword)) {
+        $result = "Your account could not be created. Please check your input.";
+    } else if (add_user($firstName, $lastName, $emailAddress, $userName, $password, $result)) {
+        $result = "The account was created successfully.";
+        $success = true;
+    }
 }
 
-$json = "{ \"$result\": true }";
+$json = json_encode([
+    "result" => $result,
+    "success" => $success
+]);
 echo($json);
 
 function add_user($firstName, $lastName, $emailAddress, $userName, $password, &$result) {
@@ -44,7 +46,7 @@ function add_user($firstName, $lastName, $emailAddress, $userName, $password, &$
     if ($statement->execute([':username' => $userName])) {
         $row = $statement->fetch();
         if ($row[0] > 0) {
-            $result = "usernameExists";
+            $result = "The username you selected is already being used.";
             return false;
         }
     }
