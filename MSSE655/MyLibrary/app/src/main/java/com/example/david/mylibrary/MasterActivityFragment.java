fdcb5a036e7fbd3d213dd72f12771c01afcf70be
David <prprcupofcoffee@gmail.com>
2017-05-24 18:05:33 -0600

    wip a/o 605pm 5/24

--- MSSE655/MyLibrary/app/src/main/java/com/example/david/mylibrary/MasterActivityFragment.java
+++ MSSE655/MyLibrary/app/src/main/java/com/example/david/mylibrary/MasterActivityFragment.java
@@ -5,11 +5,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MasterActivityFragment extends Fragment {
+
+    // storage for the list of bookcases and the associated View
+    //
+    private List<String> bookcaseNames = null;
+    private ListView bookcasesListView = null;
 
     public MasterActivityFragment() {
     }
@@ -17,6 +28,26 @@ public class MasterActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_master, container, false);
+
+        // get the view so it can be populated
+        //
+        View rootView = inflater.inflate(R.layout.fragment_master, container, false);
+
+        // load the names of the available bookcases into an Adapter
+        // so it can be bound to the view
+        //
+        bookcaseNames = new ArrayList<String>(
+                Arrays.asList(getResources().getStringArray(R.array.bookcases_list)));
+        ArrayAdapter<String> bookcaseNamesAdapter = new ArrayAdapter<>(
+                rootView.getContext(), android.R.layout.simple_list_item_1, bookcaseNames);
+
+        // grab the view and give it something to show
+        //
+        bookcasesListView = (ListView) rootView.findViewById(R.id.bookcases_listView);
+        bookcasesListView.setAdapter(bookcaseNamesAdapter);
+
+        // provide the instantiated list view
+        //
+        return rootView;
     }
 }
